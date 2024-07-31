bdac809b326549b5ab5d78b6fdf029d0a616e2b7
olawale-o <omoogunolawale@gmail.com>
2024-07-31 15:46:56 +0100

    Update style

--- src/app/team/components/Teams.tsx
+++ src/app/team/components/Teams.tsx
@@ -51,56 +51,52 @@ const teamButtons = [
 const Teams = () => {
   return (
     <section>
-      <div className="max-w-[1758px] mx-auto">
-        <div className="px-4 xl:px-20 2xl:px-[162px]">
-          <div id="tab-container">
-            <ul className="list-none flex justify-between overflow-x-scroll p-5 bg-green-20 rounded-bl-[10px] rounded-br-[10px] xl:px-20 xl:py-[30px]  2xl:py-[30px] 2xl:px-[150px]">
-              {teamButtons.map((teamButton) => (
-                <li className="" key={teamButton.id}>
-                  <button
-                    type="button"
-                    className={`py-3 px-[18px] ${teamButton.id === 1 ? "bg-green-25 border-green-30" : ""} rounded-[6px] border-solid border-[1px] border-transparent text-white text-sm font-semibold leading-[21px] whitespace-nowrap xl:text-base xl:leading-6 2xl:text-lg 2xl:leading-[27px]`}
-                  >
-                    {teamButton.title}
-                  </button>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-[30px] xl:mt-10 2xl:mt-[50px]">
-              <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-                {teams.map((team) => (
-                  <div className="col-span-1" key={team.id}>
-                    <div className="relative">
-                      <div className="pb-[60px]">
-                        <figure className="relative w-full h-full aspect-square rounded-lg bg-red-900">
-                          <div className="absolute w-full h-full inset-0 rounded-lg">
-                            <img
-                              src={team.img}
-                              className="absolute inset-0 w-full h-full block object-cover rounded-lg"
-                            />
-                          </div>
-                        </figure>
+      <div id="tab-container">
+        <ul className="list-none flex justify-between overflow-x-scroll p-5 bg-green-20 rounded-bl-[10px] rounded-br-[10px] xl:px-20 xl:py-6 2xl:py-[30px] 2xl:px-[150px] 2xl:overscroll-none">
+          {teamButtons.map((teamButton) => (
+            <li key={teamButton.id}>
+              <button
+                type="button"
+                className={`py-3 px-[18px] ${teamButton.id === 1 ? "bg-green-25 border-green-30" : ""} rounded-[6px] border-solid border-[1px] border-transparent text-white text-sm font-semibold leading-[21px] whitespace-nowrap xl:text-base xl:leading-6 2xl:text-lg 2xl:leading-[27px]`}
+              >
+                {teamButton.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-[30px] xl:mt-10 2xl:mt-[50px]">
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+            {teams.map((team) => (
+              <div className="col-span-1" key={team.id}>
+                <div className="relative">
+                  <div className="pb-[60px]">
+                    <figure className="relative w-full h-full aspect-square rounded-lg bg-red-900">
+                      <div className="absolute w-full h-full inset-0 rounded-lg">
+                        <img
+                          src={team.img}
+                          className="absolute inset-0 w-full h-full block object-cover rounded-lg"
+                        />
                       </div>
+                    </figure>
+                  </div>
 
-                      <div className="absolute z-10  bottom-0 w-full pr-[60px]">
-                        <div className="bg-green-95 rounded-tr-[20px] p-[20px]">
-                          <div>
-                            <div className="text-grey-15 text-[20px] font-semibold leading-7 whitespace-nowrap 2xl:text-2xl 2xl:leading-7">
-                              {team.name}
-                            </div>
-                            <div className="mt-2">
-                              <div className="text-grey-30 font-medium text-base whitespace-nowrap 2xl:tex-lg 2xl:leading-6">
-                                {team.role}
-                              </div>
-                            </div>
+                  <div className="absolute z-10  bottom-0 w-full pr-[60px]">
+                    <div className="bg-green-95 rounded-tr-[20px] p-[20px]">
+                      <div>
+                        <div className="text-grey-15 text-[20px] font-semibold leading-7 whitespace-nowrap 2xl:text-2xl 2xl:leading-7">
+                          {team.name}
+                        </div>
+                        <div className="mt-2">
+                          <div className="text-grey-30 font-medium text-base whitespace-nowrap 2xl:tex-lg 2xl:leading-6">
+                            {team.role}
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                ))}
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -6,19 +6,25 @@ import "./page.css";
 export default function Team() {
   return (
     <main>
-      <Header
-        heading="Meet Our Team of Experts"
-        subHeading="Our team at Nutritionist is composed of highly skilled
-      professionals who are passionate about helping you achieve
-      your health and wellness goals. With a diverse range of
-      expertise in nutrition, coaching, and support, our team is
-      dedicated to providing you with the guidance and
-      personalized care you need. Get to know the experts behind
-      our success and discover how they can make a positive
-      impact on your journey to better health."
-      />
-      <Teams />
-      <JoinOurTeam />
+      <div className="my-[50px] xl:my-[80px] 2xl:my-[100px]">
+        <div className="max-w-[1758px] mx-auto">
+          <div className="px-4 xl:px-20 2xl:px-[162px]">
+            <Header
+              heading="Meet Our Team of Experts"
+              subHeading="Our team at Nutritionist is composed of highly skilled
+                professionals who are passionate about helping you achieve
+                your health and wellness goals. With a diverse range of
+                expertise in nutrition, coaching, and support, our team is
+                dedicated to providing you with the guidance and
+                personalized care you need. Get to know the experts behind
+                our success and discover how they can make a positive
+                impact on your journey to better health."
+            />
+            <Teams />
+            <JoinOurTeam />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
