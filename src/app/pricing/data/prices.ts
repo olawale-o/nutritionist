0c8d5f844b7cff4ec921395305b532fcb9dc9402
olawale-o <omoogunolawale@gmail.com>
2024-09-29 15:26:46 +0100

    Update pricing sections of pages

--- src/app/pricing/data/prices.ts
+++ src/app/pricing/data/prices.ts
@@ -1,42 +1,83 @@
-const plans = [
-  {
-    id: 1,
-    name: "Basic Plan",
-    items: [
-      "Personalized nutrition plan tailored to your goals and dietary preferences.",
-      "Access to our mobile app for convenient meal tracking and progress monitoring.",
-      "Email support to address your questions and concerns.",
-      "Regular check-ins with a dedicated nutritionist to review your progress and provide guidance.",
-    ],
-    price: "49",
-  },
-  {
-    id: 2,
-    name: "Premium  Plan",
-    items: [
-      "All the features included in the Basic Plan.",
-      "One-on-one video consultations with your dedicated nutritionist for more personalized guidance and support.",
-      "Recipe recommendations and meal planning assistance.",
-      "Priority email support for quicker responses to your inquiries.",
-      "Educational resources and guides to deepen your understanding of nutrition and healthy habits.",
-    ],
-    price: "79",
-  },
-  {
-    id: 3,
-    name: "Ultimate  Plan",
-    items: [
-      "All the features included in the Plus Plan.",
-      "Unlimited access to video consultations with your dedicated nutritionist for ongoing support and accountability.",
-      "Advanced progress tracking tools to monitor your weight, body measurements, and fitness goals.",
-      "Customized meal plans and recipe suggestions based on your preferences and nutritional needs.",
-      "Priority email and phone support for immediate assistance.",
-    ],
-    extra:
-      "The Premium Plan is designed for individuals who are committed to achieving significant results and require the highest level of support and personalization.",
-    price: "99",
-  },
-];
+const plans = {
+  monthly: [
+    {
+      id: 1,
+      name: "Basic Plan",
+      items: [
+        "Personalized nutrition plan tailored to your goals and dietary preferences.",
+        "Access to our mobile app for convenient meal tracking and progress monitoring.",
+        "Email support to address your questions and concerns.",
+        "Regular check-ins with a dedicated nutritionist to review your progress and provide guidance.",
+      ],
+      price: "49",
+    },
+    {
+      id: 2,
+      name: "Premium  Plan",
+      items: [
+        "All the features included in the Basic Plan.",
+        "One-on-one video consultations with your dedicated nutritionist for more personalized guidance and support.",
+        "Recipe recommendations and meal planning assistance.",
+        "Priority email support for quicker responses to your inquiries.",
+        "Educational resources and guides to deepen your understanding of nutrition and healthy habits.",
+      ],
+      price: "79",
+    },
+    {
+      id: 3,
+      name: "Ultimate  Plan",
+      items: [
+        "All the features included in the Plus Plan.",
+        "Unlimited access to video consultations with your dedicated nutritionist for ongoing support and accountability.",
+        "Advanced progress tracking tools to monitor your weight, body measurements, and fitness goals.",
+        "Customized meal plans and recipe suggestions based on your preferences and nutritional needs.",
+        "Priority email and phone support for immediate assistance.",
+      ],
+      extra:
+        "The Premium Plan is designed for individuals who are committed to achieving significant results and require the highest level of support and personalization.",
+      price: "99",
+    },
+  ],
+  yearly: [
+    {
+      id: 1,
+      name: "Basic Plan",
+      items: [
+        "Personalized nutrition plan tailored to your goals and dietary preferences.",
+        "Access to our mobile app for convenient meal tracking and progress monitoring.",
+        "Email support to address your questions and concerns.",
+        "Regular check-ins with a dedicated nutritionist to review your progress and provide guidance.",
+      ],
+      price: "49",
+    },
+    {
+      id: 2,
+      name: "Premium  Plan",
+      items: [
+        "All the features included in the Basic Plan.",
+        "One-on-one video consultations with your dedicated nutritionist for more personalized guidance and support.",
+        "Recipe recommendations and meal planning assistance.",
+        "Priority email support for quicker responses to your inquiries.",
+        "Educational resources and guides to deepen your understanding of nutrition and healthy habits.",
+      ],
+      price: "79",
+    },
+    {
+      id: 3,
+      name: "Ultimate  Plan",
+      items: [
+        "All the features included in the Plus Plan.",
+        "Unlimited access to video consultations with your dedicated nutritionist for ongoing support and accountability.",
+        "Advanced progress tracking tools to monitor your weight, body measurements, and fitness goals.",
+        "Customized meal plans and recipe suggestions based on your preferences and nutritional needs.",
+        "Priority email and phone support for immediate assistance.",
+      ],
+      extra:
+        "The Premium Plan is designed for individuals who are committed to achieving significant results and require the highest level of support and personalization.",
+      price: "99",
+    },
+  ],
+};
 
 const basic = [
   true,
