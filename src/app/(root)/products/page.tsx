e5051892a3109f87557303f1ef2df284009e3f6d
olawale-o <omoogunolawale@gmail.com>
2024-09-30 11:29:38 +0100

    Implement sequential fetching

---
+++ src/app/(root)/products/page.tsx
@@ -0,0 +1,24 @@
+import { ProductPage } from "@/components/Product";
+
+function unsafe_createSequentialProcesses<T extends any[], R>(
+  ...processes: [
+    (arg?: any) => Promise<T[0]>,
+    ...((arg: any) => Promise<any>)[],
+  ]
+): Promise<R>[] {
+  return processes.reduce((acc, process, index) => {
+    if (index === 0) {
+      return [process(undefined)];
+    }
+    return [...acc, acc[acc.length - 1].then(process)];
+  }, [] as Promise<any>[]);
+}
+
+export default function Page() {
+  const [first, second, third] = unsafe_createSequentialProcesses(
+    () => new Promise((resolve) => setTimeout(() => resolve("First"), 5000)),
+    () => new Promise((resolve) => setTimeout(() => resolve("Second"), 10000)),
+    () => new Promise((resolve) => setTimeout(() => resolve("Thirds"), 15000)),
+  );
+  return <ProductPage first={first} second={second} third={third} />;
+}
