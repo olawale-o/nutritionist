472d0df1f8519df4c257fbe823f9ef687dd5bdb9
olawale-o <omoogunolawale@gmail.com>
2024-10-02 14:02:16 +0100

    Create route handler

--- src/app/(root)/products/page.tsx
+++ src/app/(root)/products/page.tsx
@@ -1,4 +1,12 @@
+import { redirect } from "next/navigation";
 import { ProductPage } from "@/components/Product";
+import { getLoggedInUser } from "@/lib/server/appwrite";
+import {
+  createNewsletterSubscriber,
+  getNewsletterSubscriber,
+} from "@/lib/server/appwrite/orm";
+import { ErrorBoundaryWithFallback } from "@/components/Error";
+import { Suspense } from "react";
 
 function unsafe_createSequentialProcesses<T extends any[], R>(
   ...processes: [
@@ -14,11 +22,32 @@ function unsafe_createSequentialProcesses<T extends any[], R>(
   }, [] as Promise<any>[]);
 }
 
-export default function Page() {
-  const [first, second, third] = [
-    new Promise((resolve) => setTimeout(() => resolve("First"), 5000)),
-    new Promise((resolve) => setTimeout(() => resolve("Second"), 10000)),
-    new Promise((resolve) => setTimeout(() => resolve("Thirds"), 15000)),
-  ];
-  return <ProductPage first={first} second={second} third={third} />;
+export default async function Page() {
+  // const [first, third] = [
+  //   new Promise((resolve) => setTimeout(() => resolve("First"), 5000)),
+  //   new Promise((resolve) => setTimeout(() => resolve("Second"), 10000)),
+  //   new Promise((resolve) => setTimeout(() => resolve("Thirds"), 15000)),
+  // ];
+
+  const user = await getLoggedInUser();
+
+  if (!user) redirect("/auth/sign-in");
+  // const first = getNewsletterSubscriber(
+  //   user.email,
+  //   process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || "",
+  // );
+  // const second = createNewsletterSubscriber(
+  //   { email: user.email },
+  //   process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || "",
+  // );
+
+  // return <ProductPage second={second} />;
+  //
+  return (
+    <ErrorBoundaryWithFallback>
+      <Suspense fallback={<div>Loading</div>}>
+        <ProductPage />
+      </Suspense>
+    </ErrorBoundaryWithFallback>
+  );
 }
