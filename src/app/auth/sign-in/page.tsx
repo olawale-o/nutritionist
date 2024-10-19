75d8f33b4251d960815b615a745d19369d7ef61a
olawale-o <omoogunolawale@gmail.com>
2024-10-19 11:30:51 +0100

    Remove unused folders

--- src/app/auth/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInPage } from "@/components/Auth/Sign-In";
-import { getLoggedInUser } from "@/lib/server/appwrite";
-import { redirect } from "next/navigation";
-
-export default async function Page() {
-  const user = await getLoggedInUser();
-  if (user) redirect("/uploads");
-  return <SignInPage />;
-}
